96b8b2da8cd190dc3b3ee1852844ca1fbfc2dc8d
joyannavb <joyannavb@gmail.com>
2022-07-31 21:17:22 -0500

    first commit

---
+++ classesExerciseOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesExerciseOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var prius = new Car();
+            prius.Make = "Toyota";
+            prius.Model = "Prius";
+            prius.Year = 2016;
+
+            Console.WriteLine($"{prius.Make} {prius.Model} {prius.Year}");
+
+        }
+    }
+}
